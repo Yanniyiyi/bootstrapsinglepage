a8fd1f53c14cddc08d36845ed351909627294a67
yanni <oneyoungyanni@gmail.com>
2015-12-03 23:20:41 +1100

    second commit

---
+++ js/myscript.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    "use strict";
+    var topoffset = 50;
+
+    $('#featured').carousel({
+        interval: false
+    });
+
+
+
+    //Activate Scorllspy
+    $('body').scrollspy({
+        target: 'header .navbar',
+        offset: topoffset
+    });
+    
+    //Add inbody class to nav
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function () {
+        var hash = $(this).find('li.active a').attr('href');
+        if (hash !== '#featured') {
+            $('header nav').toggleClass('inbody');
+        } else {
+            $('header nav').toggleClass('inbody');
+        }
+    });
+
+
+});
